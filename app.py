698f89ad17073b2f1b26a4d20274996791d96016
Parvinder123456 <124658680+Parvinder123456@users.noreply.github.com>
2025-08-09 00:58:18 +0530

    new texts

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+import re
 
 load_dotenv()
 
@@ -10,6 +11,11 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 @app.event("app_mention")
 def handle_app_mention_event(body,say):
     say("Yes? How can I help you today?")
+
+@app.message(re.compile("(fuck)"))
+def fuckhandler(say,context):
+    greeting = context['matches'][0]
+    say(f"{greeting} you motherfucker")
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app,os.environ.get("SLACK_APP_TOKEN"))
