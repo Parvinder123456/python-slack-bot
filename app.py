3f41bac64126bbe150862c1354f6eea8727d9d5c
Parvinder123456 <124658680+Parvinder123456@users.noreply.github.com>
2025-08-09 01:15:15 +0530

    new changes

--- app.py
+++ app.py
@@ -8,14 +8,16 @@ load_dotenv()
 
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
-@app.event("app_mention")
-def handle_app_mention_event(body,say):
-    say("Yes? How can I help you today?")
-
 @app.message(re.compile("(fuck)"))
 def fuckhandler(say,context):
     greeting = context['matches'][0]
     say(f"{greeting} you motherfucker")
+
+@app.event("app_mention")
+def handle_app_mention_event(body,say):
+    say("Yes? How can I help you today?")
+
+
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app,os.environ.get("SLACK_APP_TOKEN"))
